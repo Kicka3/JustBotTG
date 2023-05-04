a9f85f5bda4d27c42894591e2d4579f01f53776a
Ozornin Kirill <kirill_ozornin@mail.ru>
2023-05-04 19:41:58 +0500

    exm2

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const TelegramApi = require('node-telegram-bot-api');
-const {gameOptions, againOptions, genderOptions} = require('./options.js')
+const { gameOptions, againOptions, genderOptions } = require('./options.js');
 const token = '6215268536:AAE9O_jZcfrOjpAX5vizfeuSMxX-bP99778'
 
-const bot = new TelegramApi(token, { polling: true })
-
-const gender = {}
+const bot = new TelegramApi(token, { polling: true });
 
 const chats = {}
 
@@ -28,74 +26,53 @@ const start = () => {
       { command: `/game`, description: 'Давай поиграем!' },
       { command: `/compliments`, description: 'Раздел комплиментов и поддержки' },
 
-   ]);
-
-   bot.on('message', async msg => {
-      const text = msg.text;
-      const chatId = msg.chat.id;
-
-      if (text === '/start') {
-         await bot.sendSticker(chatId, `https://tlgrm.eu/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/1.jpg`);
-         return bot.sendMessage(chatId, 'Ты написал мне, классно!');
-      }
-      if (text === '/info') {
-         return bot.sendMessage(chatId, `Тебя зовут --> ${msg.from.first_name} , твой ник --> ${msg.from.username}`);
-      }
-      if (text === '/game') {
-         return startGame(chatId);
-      }
-      if (text === '/compliments') {
-         return compliments(chatId)
-      }
-
-
-      //Эта хуйня не работает
-      const data = msg.data;
-      const ChatId = msg.message.chat.id;
-      if (genderData === gender[chatId]) {
-         return bot.sendMessage(chatId, `Ты мужик блеат! Соберись, ${msg.from.username}, не будь тряпкой!`)
-      } else {
-         return bot.sendMessage(chatId, `Эти комплименты должны тебе помочь`)
-         console.log(msg)
-      }
-
-
-
-      return bot.sendMessage(chatId, `Я тебя не понимаю, попробуй ещё раз`)
-
-   });
-
-   bot.on('callback_query', async msg => {
-      const data = msg.data;
-      const chatId = msg.message.chat.id;
-      if (data === '/again') {
-         return startGame(chatId);
-      }
-      if (data === chats[chatId]) {
-         return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]} !`, againOptions)
-      } else {
-         return bot.sendMessage(chatId, `Ох и ах, ты не угадал цифру.. Это была цифра ${chats[chatId]} !`, againOptions)
-      }
-
-
-   });
-
-
+   ])
 }
+
+
+
+bot.on('message', async msg => {
+   const text = msg.text;
+   const chatId = msg.chat.id;
+   
+   if (text === '/start') {
+      await bot.sendSticker(chatId, `https://tlgrm.eu/_/stickers/22c/b26/22cb267f-a2ab-41e4-8360-fe35ac048c3b/192/18.webp`);
+      return bot.sendMessage(chatId, 'Ты написал мне, классно!');
+   }
+   if (text === '/info') {
+      return bot.sendMessage(chatId, `Тебя зовут --> ${msg.from.first_name} , твой ник --> ${msg.from.username}`);
+   }
+   if (text === '/game') {
+      return startGame(chatId);
+   }
+   if (text === '/compliments') {
+      return compliments(chatId)
+   }
+
+
+   const idno = await bot.sendSticker(chatId, `https://tlgrm.eu/_/stickers/22c/b26/22cb267f-a2ab-41e4-8360-fe35ac048c3b/192/16.webp`);
+   return idno, bot.sendMessage(chatId, `Я тебя не понимаю, попробуй ещё раз`)
+});
+
+
+
+bot.on('callback_query', async msg => {
+   const data = msg.data;
+   const chatId = msg.message.chat.id;
+   if (data === '/again') {
+      return startGame(chatId);
+   }
+   if (data === chats[chatId]) {
+      return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру ${chats[chatId]} !`, againOptions)
+   } else {
+      return bot.sendMessage(chatId, `Ох и ах, ты не угадал цифру.. Это была цифра ${chats[chatId]} !`, againOptions)
+   }
+});
+
 
 start()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
